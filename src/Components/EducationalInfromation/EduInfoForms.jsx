a1f0fe475e1ac82f88e7fa37b8aad8719be7d91a
ztanvir <zahirulopel@gmail.com>
2024-02-12 16:31:09 +0600

    Adjust education style and Education btn

--- src/Components/EducationalInfromation/EduInfoForms.jsx
+++ src/Components/EducationalInfromation/EduInfoForms.jsx
@@ -1,16 +1,17 @@
 import InstituteInfo from "./InstituteInfo";
+import eduInfoFormStyle from "../Styles/EduInfoStyle/eduInfoForm.module.css";
 
 const EduInfoForms = () => {
   return (
-    <section className="education-forms">
-      <h3>
+    <section className={eduInfoFormStyle.educationForms}>
+      <h3 className={eduInfoFormStyle.educationText}>
         <span>
-          <span className="education-icon">+</span>
-          <span>Education</span>
+          <span className="material-symbols-outlined">school</span> Education
         </span>
-        <span className="education-dropdown">Dropdown</span>
+        {/* expand_less */}
+        <span className="material-symbols-outlined">expand_more</span>
       </h3>
-      <button>+ Education</button>
+      <button className={eduInfoFormStyle.addEducationBtn}>+ Education</button>
       <InstituteInfo />
     </section>
   );
