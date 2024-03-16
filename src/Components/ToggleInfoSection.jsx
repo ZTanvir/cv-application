be4a587d78db2b60eb58fa1974011375fd883ea4
ztanvir <zahirulopel@gmail.com>
2024-03-16 12:45:04 +0600

    Toggle Info component show or hide information of the child

--- src/Components/ToggleInfoSection.jsx
+++ src/Components/ToggleInfoSection.jsx
@@ -1,17 +1,36 @@
+import { useState } from "react";
 import toggleInfoStyle from "./Styles/toggleInfoSection.module.css";
 
 const ToggleInfoSection = ({ children, icon, sectionName }) => {
+  const [showDetails, setshowDetails] = useState(false);
+
+  const expandOrHideCard = (event) => {
+    // show or hide additional information about this section
+    setshowDetails(!showDetails);
+  };
+
   return (
     <section className={sectionName}>
-      <h3 className={toggleInfoStyle.sectionDetails}>
+      <h3 onClick={expandOrHideCard} className={toggleInfoStyle.sectionDetails}>
         <span>
           <span className="material-symbols-outlined">{icon}</span>
           {sectionName}
         </span>
         {/* expand_less */}
-        <span className="material-symbols-outlined">expand_more</span>
+        <span className="material-symbols-outlined">
+          {showDetails ? "expand_less" : "expand_more"}
+        </span>
       </h3>
-      <div>{children}</div>
+      {/* this div will be show or hide */}
+      <div
+        className={
+          showDetails
+            ? toggleInfoStyle.showSection
+            : toggleInfoStyle.hideSection
+        }
+      >
+        {children}
+      </div>
     </section>
   );
 };
