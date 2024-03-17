f26c91fddbe334e7652a32883beaf683896c6477
ztanvir <zahirulopel@gmail.com>
2024-03-17 12:23:45 +0600

    Add ToggleButton component to show or hide section on btn click

--- src/Components/EducationalInfromation/EduInfoForms.jsx
+++ src/Components/EducationalInfromation/EduInfoForms.jsx
@@ -1,15 +1,22 @@
 import InstituteInfo from "./InstituteInfo";
 import eduInfoFormStyle from "../Styles/EduInfoStyle/eduInfoForm.module.css";
 import ToggleInfoSection from "../ToggleInfoSection";
+import ToggleButton from "../ToggleButton";
+import { useState } from "react";
 
 const EduInfoForms = () => {
+  const [toggleInstituteInfo, setToggleInstituteInfo] = useState(false);
+
+  const handleEducation = (event) => {
+    setToggleInstituteInfo(!toggleInstituteInfo);
+  };
+
   return (
     <section className={eduInfoFormStyle.educationForms}>
       <ToggleInfoSection icon="school" sectionName="Education">
-        <button className={eduInfoFormStyle.addEducationBtn}>
-          + Education
-        </button>
-        <InstituteInfo />
+        <ToggleButton buttonName="Education" addInfoBtnOnlick={handleEducation}>
+          {toggleInstituteInfo ? <InstituteInfo /> : null}
+        </ToggleButton>
       </ToggleInfoSection>
     </section>
   );
--- src/Components/ToggleButton.jsx
+++ src/Components/ToggleButton.jsx
@@ -1,14 +1,23 @@
-import toggleButtonStyle from "./Styles/ToggleButton.jsx";
+import { useState } from "react";
+import toggleButtonStyle from "./Styles/toggleButton.module.css";
 
-const ToggleButton = ({ children, buttonName }) => {
+const ToggleButton = ({ children, buttonName, addInfoBtnOnlick }) => {
   return (
     <>
       <section className={toggleButtonStyle.toggleDetails}>
-        <button>
-          {/* Add material symbols*/}
-          <span class="material-symbols-outlined">add</span>
-          {buttonName}
-        </button>
+        <section className={toggleButtonStyle.btnSection}>
+          <button
+            onClick={addInfoBtnOnlick}
+            className={toggleButtonStyle.addBtn}
+          >
+            {/* Add material symbols*/}
+            <span className="material-symbols-outlined">add</span>
+            {buttonName}
+          </button>
+        </section>
+        <section className={toggleButtonStyle.btnChildSection}>
+          {children}
+        </section>
       </section>
     </>
   );
